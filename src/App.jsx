b439816d381cd299afdc4bc1e86901622423944d
asfakT <asfak.shahrier@workslayr.com>
2025-06-20 14:21:13 +0600

    fix: about us issue resolve

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Navigation from './components/Navigation/Navigation';
 import Hero from './components/Hero/Hero';
 import ContactUs from './components/ContactUs/ContactUs';
 import DiceGame from './components/DiceGame/DiceGame';
-import AboutUs from './components/AboutUs/aboutUs';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 
@@ -27,7 +26,7 @@ const App = () => {
         <Route path="/" element={<Hero />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/dicegame" element={<DiceGame />} />
-        <Route path="/about" element={<AboutUs />} />
+       
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login setUser={setUser} />} />
       </Routes>
